6b6eefb0ecfce572b1e537db899af5f5deada85d
Tom Hawes <twhawes@gmail.com>
2020-03-09 21:51:05 +0000

    set up search jsx component

--- nc-news-fe/src/Components/Header.jsx
+++ nc-news-fe/src/Components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Link} from '@reach/router'
+import Search from './Search';
 
 const Header = () => {
     return (
         <header className='headerBar'>
             <h1  className='heading'><Link to ='/'>NC News</Link></h1> 
+            <Search />
             <nav className='navBar'>
             <h2><Link to='/articles/topic/football'>Football</Link> <span></span><span></span><Link to='/articles/topic/coding'>Coding</Link> <span></span><span></span> <Link to='/articles/topic/cooking'>   Cooking</Link></h2> 
             </nav>
---
+++ nc-news-fe/src/Components/Search.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    state = {
+        searchInput: '',
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Search;
